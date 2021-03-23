12435de48b3292db28980fd1697cace985b70482
Kevin Moore <moorekevin711@gmail.com>
2021-03-23 14:43:16 -0600

    path changes

--- runiso.jl
+++ runiso.jl
@@ -37,27 +37,27 @@ end
 function turbine_validation_isosurface()
 
     # File start and stop
-    file_start=1;
-    file_end=10;
+    file_start=46;
+    file_end=80;
 
     # Define bounds
     
-    # bounds=[[-10.0,-0.5,-5.0],[0.5,0.5,5.0],[21,3,21]];
+    bounds=[[-10.0,-0.5,-6.0],[0.5,0.5,6.0],[21,3,21]];
 
-    bounds=[[-10.0,-5.0,-5.0],[0.5,5.0,5.0],[17,17,17]];
+    #bounds=[[-10.0,-5.0,-5.0],[0.5,5.0,5.0],[17,17,17]];
 
 
     # Define freestream
     freestream = [-20.0,0.0,0.0];
 
     # Data read
-    data_path="/media/flowlab/Storage/gdevenport/simulations/turbine_validation20_copy/";
+    data_path="/media/flowlab/Storage/gdevenport/simulations/turbine_validation20/";
     pfield_file_name = "sim_pfield"
 
     # Data write
-    save_path = "/media/flowlab/Storage/gdevenport/simulations/vtk/turbine_output/";
+    save_path = "/media/flowlab/Storage/gdevenport/simulations/Vorticity_validation/";
     pfield_save_name = "turbine_validation20";
-    vtk_save_name = "turbine_validation20";
+    vtk_save_name = "turbine_validation20_highrez";
         
     create_isosurface(
         file_start=file_start,
@@ -111,11 +111,11 @@ function single_rotor_isosurface()
 
     # File start and stop
     file_start=1;
-    file_end=10;
+    file_end=40;
 
     # Define bounds
-    # bounds=[[-0.02,-0.001,-0.15],[0.2,0.001,0.15],[20,5,20]];
-    bounds=[[-0.02,-0.15,-0.15],[0.2,0.15,0.15],[17,17,17]];
+    bounds=[[-0.2,-0.001,-0.35],[0.4,0.001,0.35],[20,5,20]]; # Added 0.2 to all parameteres except the y components. 
+    #bounds=[[-0.02,-0.15,-0.15],[0.2,0.15,0.15],[17,17,17]];
 
     # Define freestream
     freestream = [0.0,0.0,0.0];
@@ -125,7 +125,7 @@ function single_rotor_isosurface()
     pfield_file_name = "singlerotor_pfield"
 
     # Data write
-    save_path = "/media/flowlab/Storage/gdevenport/simulations/single_rotor_output/";
+    save_path = "/media/flowlab/Storage/gdevenport/simulations/Vorticity_validation/";
     pfield_save_name = "single_rotor";
     vtk_save_name = "single_rotor";
         
@@ -142,6 +142,72 @@ function single_rotor_isosurface()
         ) 
 end
 
-turbine_validation_isosurface()
+function phantom_isosurface()
+    # File start and stop
+    file_start=1;
+    file_end=80;
 
-single_rotor_isosurface()
+    # Define bounds
+    bounds=[[-0.1,-0.2,-0.2],[0.25,0.2,0.2],[20,5,20]];
+
+    # Define freestream
+    freestream = [0.0,0.0,0.0];
+
+    # Data read
+    data_path="/media/flowlab/Storage/gdevenport/simulations/phantom_hover/singlerotor_hover_test00/";
+    pfield_file_name = "singlerotor_pfield"
+
+    # Data write
+    save_path = "/media/flowlab/Storage/gdevenport/simulations/Vorticity_validation/";
+    pfield_save_name = "phantom";
+    vtk_save_name = "phantom";
+        
+    create_isosurface(
+        file_start=file_start,
+        file_end=file_end,
+        bounds=bounds,
+        freestream=freestream,
+        data_path=data_path,
+        pfield_file_name=pfield_file_name,
+        save_path=save_path,
+        pfield_save_name=pfield_save_name,
+        vtk_save_name=vtk_save_name
+        ) 
+
+end
+
+function phantom_flight_isosurface()
+    # File start and stop
+    file_start=1;
+    file_end=80;
+
+    # Define bounds
+    bounds=[[0.0,-0.12,-0.12],[0.25,0.12,0.12],[20,5,20]];
+
+    # Define freestream
+    freestream = [0.0,0.0,0.0];
+
+    # Data read
+    data_path="/media/flowlab/Storage/gdevenport/simulations/data/phantom/singlerotor_fflight_test01/";
+    pfield_file_name = "singlerotor_pfield"
+
+    # Data write
+    save_path = "/media/flowlab/Storage/gdevenport/simulations/isosurface/phantom flight/";
+    pfield_save_name = "phantom";
+    vtk_save_name = "phantom";
+        
+    create_isosurface(
+        file_start=file_start,
+        file_end=file_end,
+        bounds=bounds,
+        freestream=freestream,
+        data_path=data_path,
+        pfield_file_name=pfield_file_name,
+        save_path=save_path,
+        pfield_save_name=pfield_save_name,
+        vtk_save_name=vtk_save_name
+        ) 
+
+end
+
+phantom_flight_isosurface()
